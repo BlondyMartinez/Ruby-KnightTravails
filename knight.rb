fae58c35ea77294f910bb7ab1ea2a437f41e39c5
Blondy <blondymartinezm@gmail.com>
2024-03-02 04:21:05 +0100

    added bfs

--- knight.rb
+++ knight.rb
@@ -1,4 +1,8 @@
+require 'set'
+
 class Knight
+    attr_reader :path
+
     MOVE_OFFSETS = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [-2, -1], [-2, 1],  [2, -1]]
 
     def initialize (start, destination)
@@ -12,9 +16,32 @@ class Knight
         MOVE_OFFSETS.each do |offset|
             x = position[0] + offset [0]
             y = position[1] + offset [1]
-            possible_moves.push[x, y] if x.between?(0, 7) && y.between?(0, 7)
+            possible_moves.push([x, y]) if x.between?(0, 7) && y.between?(0, 7) 
         end
 
         possible_moves
+    end
+
+    def knight_moves
+        queue = [[@start]]
+        visited = Set.new([@start])
+
+        until queue.empty?
+            @path = queue.shift
+            current_position = @path.last
+
+            if current_position == @destination
+                return @path
+            end
+
+            generate_possible_moves(current_position).each do |next_position|
+                unless visited.include?(next_position)
+                    visited.add(next_position)
+                    queue << @path + [next_position]
+                end
+            end
+        end
+        
+        return nil
     end
 end

6d806657b25873045cc9fe226dcfd8734a951c4b
Blondy <blondymartinezm@gmail.com>
2024-03-02 04:20:12 +0100

    added add_mark_to_board

--- board.rb
+++ board.rb
@@ -1,8 +1,8 @@
+require_relative 'color'
+
 class Board
-    attr_reader :board
 
     def initialize
-        # 3x3
         @board = Array.new(8) { Array.new(8, ' ') }
         @letters = ('A'..'H').to_a
     end
@@ -10,7 +10,7 @@ class Board
     def display
         letter_index = @letters.length - 1
 
-        @board.each_with_index do |row, index|
+        @board.reverse.each_with_index do |row, index|
             puts "#{@letters[letter_index]}   " + row.join(' | ') + ' '
             puts '   ---|---|---|---|---|---|---|---' if index != @board.length - 1
             letter_index -= 1
@@ -18,5 +18,11 @@ class Board
 
         
         puts "\n    1   2   3   4   5   6   7   8\n"
+    end
+
+    def add_mark_to_board(coordinates, color = nil)
+        x, y = coordinates
+        colored_mark = color ? '●'.send(color) : '●'
+        @board[x][y] = colored_mark
     end
 end
